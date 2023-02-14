09ee88bbd02522f9eb4f2ea233546f8349d4219e
unknown <nhazetsu@gmail.com>
2023-02-14 21:15:53 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐Hello người em yêu!🖐',
+    introDesc: `Em có điều này muốn hỏi chị, chị nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Em biết chị thích em phải không phải không ? 😙',
+    desc: 'Chị không trả lời hoặc thoát ra, tức là Chị thích em đó nha :> ',
+    btnYes: 'Có, chị thật sự thích Em 💖💖💖',
+    btnNo: '💔 Cưng mơ à ! 💔 ',
+    question:'Nói cho em biết đi vì sao chị thích em 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Chị đùa đó, chị không có thích em đâu. 😊😊',
+    mess: 'Em biết mà, chị chưa bao giờ thích em mà 🥰. ',
+    messDesc: '💬💬 Nhắn tin cho em nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/Monster.CST'
+}
